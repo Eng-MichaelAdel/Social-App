ce746e9782c1f7a5588d0b57dbf82e9460c5f21e
Eng-MichaelAdel <engmichael89@gmail.com>
2026-05-29 21:43:59 +0300

    feat:initialize the express app

--- src/app.bootstrap.ts
+++ src/app.bootstrap.ts
@@ -1,6 +1,25 @@
+import express, { Application } from "express";
+import { envConfig } from "./Config";
+
 const bootstrap = () => {
-  let x: string = "maik";
-  console.log(x);
+  const app: Application = express();
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  const port: string | number = envConfig.app.PORT;
+  app.listen(port, () => {
+    console.log(`server is running on port ::: ${port} 🚀🚀`);
+  });
 };
 
-export default bootstrap
+export default bootstrap;
